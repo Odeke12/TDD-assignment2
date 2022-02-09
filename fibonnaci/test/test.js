491c00523e9e9c7354f9ad48afd52aba2b13cfd1
Odeke12 <trevodex@gmail.com>
2022-02-09 10:52:01 +0300

    New test cases

--- fibonnaci/test/test.js
+++ fibonnaci/test/test.js
@@ -2,6 +2,12 @@ const fibonnaci = require('../fibonnaci.js')
 var assert = require('chai').assert;
 
 describe('Testing fibonnaci', () => {
+    it('fib("Hello there") = undefined', () => {
+        assert.equal(fibonnaci("Hello there"),undefined);
+    })
+    it('fib(-9) = undefined', () => {
+        assert.equal(fibonnaci(-9),undefined);
+    })
     it('fib(1) = 0', () => {
         assert.equal(fibonnaci(1),0);
     })
@@ -11,7 +17,7 @@ describe('Testing fibonnaci', () => {
     it('fib(5) = 3', () => {
         assert.equal(fibonnaci(5),3);
     })
-    it('fib(8) = 13', () => {
-        assert.equal(fibonnaci(8),13);
+    it('fib(20) = 4181', () => {
+        assert.equal(fibonnaci(20),4181);
     })
 })
